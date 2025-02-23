69c4ff5d35206f801f1872852136925ecab3d130
Erinç Uysal <erincuysal@outlook.com>
2025-02-24 00:43:42 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -28,7 +28,21 @@
       '';
 
       installPhase = ''
-        make PREFIX=$out install
+        # Create a temporary directory for terminfo files
+        TERMINFO=$out/share/terminfo
+        mkdir -p $TERMINFO
+
+        # Install st binary and man page
+        mkdir -p $out/bin
+        cp -f st $out/bin
+        chmod 755 $out/bin/st
+
+        mkdir -p $out/share/man/man1
+        sed "s/VERSION/0.9.2/g" < st.1 > $out/share/man/man1/st.1
+        chmod 644 $out/share/man/man1/st.1
+
+        # Compile and install terminfo files
+        tic -sx -o $TERMINFO st.info
       '';
     };
   };
