56adccf0a73e5076a2c0d4177f502e5167026486
Erinç Uysal <erincuysal@outlook.com>
2025-02-24 00:29:08 +0300

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "My custom st (Simple Terminal) fork";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.stdenv.mkDerivation {
+      name = "st";
+      src = self;
+
+      buildInputs = with nixpkgs.legacyPackages.x86_64-linux; [
+        xorg.libX11
+        xorg.libXft
+        freetype
+        fontconfig
+      ];
+
+      prePatch = ''
+        sed -i 's@/usr/local@$out@g' config.mk
+      '';
+
+      installPhase = ''
+        make PREFIX=$out install
+      '';
+    };
+  };
+}
