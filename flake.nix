7d5618a95966491d98200ed0d4fc5ca67ef18d88
Erinç Uysal <erincuysal@outlook.com>
2025-02-24 00:39:51 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
   outputs = { self, nixpkgs }: {
     packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.stdenv.mkDerivation {
       name = "st";
-      src = self;
+      src = "${self}/src";
 
       nativeBuildInputs = with nixpkgs.legacyPackages.x86_64-linux; [
         pkg-config
